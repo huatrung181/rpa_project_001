dfbe24ceb93277dee7744a9239722c6f82de433a
Hieu <huatrunghieu181@gmail.com>
2024-06-05 09:54:48 +0700

    initial commit

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.CodedWorkflows.DescriptorIntegration;
+
+namespace UploadPo
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService)};
+        }
+
+        protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
+    }
+}
+
+namespace UploadPo.ObjectRepository
+{
+    public static class Descriptors
+    {
+    }
+}
+
+namespace UploadPo._Implementation
+{
+    internal class ScreenDescriptorDefinition : IScreenDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+
+    internal class ElementDescriptorDefinition : IElementDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public IElementDescriptor ParentElement { get; set; }
+
+        public IElementDescriptor Element { get; set; }
+    }
+}
